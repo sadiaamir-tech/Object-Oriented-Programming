9f0afd85f91d988558253f470aaf1cd9432a0407
sadiaamir-tech <sadia.amir2006@gmail.com>
2026-04-04 21:28:24 +0500

    commit

---
+++ Practise Question/Multilevel Inheritance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    void showA() {
+        cout << "Class A" << endl;
+    }
+};
+
+class B : public A {
+public:
+    void showB() {
+        cout << "Class B derived from A" << endl;
+    }
+};
+
+class C : public B {
+public:
+    void showC() {
+        cout << "Class C derived from B" << endl;
+    }
+};
+
+int main() {
+    C obj;
+    obj.showA();
+    obj.showB();
+    obj.showC();
+}
